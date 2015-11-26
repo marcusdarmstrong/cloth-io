4877669d3c30025d2b760d1930206d2328a8bf7e
Marcus Armstrong <marcusdarmstrong@gmail.com>
2015-11-26 14:43:55 -0500

    More escaping fixes

--- src/components/post.js
+++ src/components/post.js
@@ -3,6 +3,6 @@ import React from 'react';
 export default ({post}) => (
   <article className="article">
     <h1>{post.title}</h1>
-    <div dangerouslySetInnerHTML={post.body}></div>
+    <div dangerouslySetInnerHTML={{__html: post.body}}></div>
   </article>
 );
