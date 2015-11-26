2b432e4dbbea35acb83806ea971353522a5595a6
Marcus Armstrong <marcusdarmstrong@gmail.com>
2015-11-26 13:13:23 -0500

    Adding basic postgres things

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import path from 'path';
+import { native as pg } from 'pg';
 const app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -18,3 +19,18 @@ app.get('/', (req, res) => {
   res.send(layout('Hi', App));
 });
 
+app.get('/p/.*', (req, res) => {
+  pg.connect(process.env.DATABASE_URL, (pgErr, client, done) => {
+    client.query('select * from t_post', (err) => {
+      done();
+      if (pgErr || err) {
+        const error = pgErr || err;
+        console.error(error);
+        res.send('Error ' + error);
+      } else {
+        res.send(layout('Success'), App);
+      }
+    });
+  });
+  res.send(layout('The title'), App);
+});
