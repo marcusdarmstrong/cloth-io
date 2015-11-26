78ca7aea272949c934c3839edf98220b8c14820e
Marcus Armstrong <marcusdarmstrong@gmail.com>
2015-11-26 14:28:03 -0500

    Need to avoid html escaping

--- src/components/post.js
+++ src/components/post.js
@@ -3,6 +3,6 @@ import React from 'react';
 export default ({post}) => (
   <article className="article">
     <h1>{post.title}</h1>
-    {post.body}
+    <div dangerouslySetInnerHTML={post.body}></div>
   </article>
 );
