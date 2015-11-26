ba23be8224149547127d6f7634f3f79b69312d25
Marcus Armstrong <marcusdarmstrong@gmail.com>
2015-11-26 01:28:47 -0500

    Initial load of my styling work

---
+++ gulpfile.js
@@ -0,0 +1,64 @@
+'use strict';
+
+var gulp = require('gulp');
+var eslint = require('gulp-eslint');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
+var del = require('del');
+var browserify = require('browserify');
+var babelify = require('babelify');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var babel = require('gulp-babel');
+
+gulp.task('clean-server', function() {
+  del(['bin/*.js']);
+});
+
+gulp.task('clean-client', function() {
+  del(['public/*.js', 'public/*.map']);
+});
+
+gulp.task('lint', function() {
+  return gulp.src(['src/**/*.js'])
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
+});
+
+gulp.task('client', ['clean-client'], function() {
+  return browserify({ debug: true })
+    .transform(babelify)
+    .require('src/client.js', { entry: true })
+    .bundle()
+    .on('error', function handleError(err) {
+      console.error(err.toString());
+      this.emit('end');
+    })
+    .pipe(source('app.js'))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({loadMaps: true}))
+      .pipe(uglify())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('public'));
+});
+
+gulp.task('server', ['clean-server'], function() {
+  return gulp.src(['src/**/*.js'])
+    .pipe(babel())
+    .pipe(gulp.dest('bin'));
+  /*return browserify()
+    .transform(babelify)
+    .require('src/server.js', { entry: true })
+    .bundle()
+    .on('error', function handleError(err) {
+      console.error(err.toString());
+      this.emit('end');
+    })
+    .pipe(source('server.js'))
+    //.pipe(buffer())
+    .pipe(gulp.dest('bin'));*/
+});
+
+gulp.task('default', ['lint', 'server', 'client']);
---
+++ src/layout.js
@@ -0,0 +1,126 @@
+export default function(title, body) {
+  return `<!doctype html>
+<html lang="en">
+<head>
+  <link href='https://fonts.googleapis.com/css?family=Raleway:400,100,200,300,500,600,700,800,900|Open+Sans:400,600,300,300italic,400italic,600italic,700italic,800italic,800,700|Merriweather:400,300,700,300italic,900italic,700italic,400italic,900' rel='stylesheet' type='text/css'>
+  <link rel="stylesheet" href="public/app.css">
+  <meta charset="utf-8">
+  <title>${title}</title>
+</head>
+<body>
+  <nav><img class="logo" src="public/images/logo.png"><div class="button pull-right">+</div></nav>
+  <div class="container">
+    <article class="article">
+      <h1>BGA Wrap Up: Jets at Texans</h1>
+      <p>For all the changes the Jets made this offseason, yesterday's loss and the struggles over the past few weeks had a familiar feel to them.</p>
+      <p>However, if I told you at the start of the season that the Jets would be without Nick Mangold, Sheldon Richardson and Darrelle Revis during a November road game against a team leading their division, would you really have expected anything better than a competitive loss?</p>
+      <p>Right now, the Jets are victims of their own early-season success because the 4-1 start placed unrealistic expectations on the team. At the same time, they're not as bad as this 1-4 stretch would suggest either.</p>
+    </article>
+    <section class="comments">
+      <div class="comment has-replies">
+        <div class="author"><div class="avatar avatar-1">B</div></div>
+        <div class="comment-container">
+          <div class="comment-header">
+            <div class="comment-timestamp pull-right">
+              14 minutes ago
+            </div>
+            <div class="author-name">
+              Brendan
+            </div>
+          </div>
+          <div class="comment-text">
+            <p>I think one of the more frustrating aspects of the Jets recent slump over the last five games is made more alarming by the recurring pattern behind it. Team gets off to a sluggish start, looks unprepared. At least one unit... it varies week to week... seems <strong>totally</strong> lost in space and the others cannot compensate for it.</p>
+            <p>That fragility, or lack of mental toughness, surfaces, and the team falls behind early.</p>
+          </div>
+          <div class="comment-options">
+            <div class="button pull-right">Reply</div>
+          </div>
+        </div>
+      </div>
+      <div class="reply">
+        <div class="comment fork has-replies">
+          <div class="author"><div class="avatar avatar-2">A</div></div>
+          <div class="comment-container">
+            <div class="comment-header">
+              <div class="comment-timestamp pull-right">
+                10 minutes ago
+              </div>
+              <div class="author-name">
+                a57se
+              </div>
+            </div>
+            <div class="comment-text">
+              <p>You're a lying liar from liarville and I can't even read this without throwing up.</p>
+            </div>
+            <div class="comment-options">
+              <div class="button pull-right">Reply</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="reply">
+        <div class="comment child">
+          <div class="author"><div class="avatar avatar-3">T</div></div>
+          <div class="comment-container">
+            <div class="comment-header">
+              <div class="comment-timestamp pull-right">
+                5 minutes ago
+              </div>
+              <div class="author-name">
+                tsjc68
+              </div>
+            </div>
+            <div class="comment-text">
+              <p><em>Insert witty gif here.</em></p>
+            </div>
+            <div class="comment-options">
+              <div class="button pull-right">Reply</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="comment child">
+        <div class="author"><div class="avatar avatar-4">M</div></div>
+        <div class="comment-container">
+          <div class="comment-header">
+            <div class="comment-timestamp pull-right">
+              12 minutes ago
+            </div>
+            <div class="author-name">
+              Marcus
+            </div>
+          </div>
+          <div class="comment-text">
+            <p>This is my incredibly insightful reply to your comment.</p>
+          </div>
+          <div class="comment-options">
+            <div class="button pull-right">Reply</div>
+          </div>
+        </div>
+      </div>
+      <div class="comment">
+        <div class="author"><div class="avatar avatar-5">B</div></div>
+        <div class="comment-container">
+          <div class="comment-header">
+            <div class="comment-timestamp pull-right">
+              15 minutes ago
+            </div>
+            <div class="author-name">
+              Bent
+            </div>
+          </div>
+          <div class="comment-text">
+            <p>I hope you like my BGA!</p>
+          </div>
+          <div class="comment-options">
+            <div class="button pull-right">Reply</div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+  <!-- ${body} -->
+  <script src="public/app.js"></script>
+</body>
+</html>`;
+}
