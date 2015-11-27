c17cc1f4340e857801ef28c299c8817b8728855a
Marcus Armstrong <marcusdarmstrong@gmail.com>
2015-11-27 12:26:08 -0500

    Adding a spacer

--- src/components/post.js
+++ src/components/post.js
@@ -5,12 +5,14 @@ import Nav from './nav';
 export default ({post, comments}) => (
   <div>
     <Nav />
-    <div className="container">
-      <article className="article">
-        <h1>{post.title}</h1>
-        <div dangerouslySetInnerHTML={{__html: post.body}}></div>
-      </article>
-      <Comments comments={comments} />
+    <div className="spacer">
+      <div className="container">
+        <article className="article">
+          <h1>{post.title}</h1>
+          <div dangerouslySetInnerHTML={{__html: post.body}}></div>
+        </article>
+        <Comments comments={comments} />
+      </div>
     </div>
   </div>
 );
