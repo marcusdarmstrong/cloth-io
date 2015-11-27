5841af026221bb6eba3a182e0e48d2a4ec333762
Marcus Armstrong <marcusdarmstrong@gmail.com>
2015-11-27 18:19:57 -0500

    Ios fonts maybe

--- src/layout.js
+++ src/layout.js
@@ -2,11 +2,11 @@ export default function(title, body) {
   return `<!doctype html>
 <html lang="en">
 <head>
-  <link rel="stylesheet" href="/public/fonts.css">
   <link rel="stylesheet" href="/public/app.css">
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"/>
   <title>${title}</title>
+  <link href='https://fonts.googleapis.com/css?family=Raleway:400,100,200,300,500,600,700,800,900|Open+Sans:400,600,300,300italic,400italic,600italic,700italic,800italic,800,700|Merriweather:400,300,700,300italic,900italic,700italic,400italic,900' rel='stylesheet' type='text/css'>
 </head>
 <body>
   ${body}
