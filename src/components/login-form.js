aa9318dbd083eb85b5da994b85cee01560057ba5
Marcus Armstrong <marcusdarmstrong@gmail.com>
2015-11-28 13:49:57 -0500

    Added state to forms

--- src/components/login-form.js
+++ src/components/login-form.js
@@ -1,15 +1,33 @@
 import React from 'react';
 
-export default (props) => (
-  <form className="login-form">
-    <div className="create-account-cta">
-      <div className="button pull-right" onClick={props.openModal.bind(this, 'signup')}>Create account</div>
-      <div>New here?</div>
-    </div>
-    <label htmlFor="email">Email address:</label>
-    <input type="email" autoFocus name="email" />
-    <label htmlFor="password">Password:</label>
-    <input type="password" name="password" />
-    <input type="submit" name="submit" value="Log in" />
-  </form>
-);
+class LoginForm extends React.Component {
+  getInitialState() {
+    return {email: '', password: ''};
+  }
+  handleEmailChange(e) {
+    this.setState({email: e.target.value});
+  }
+  handlePasswordChange(e) {
+    this.setState({password: e.target.value});
+  }
+  handleSubmit(e) {
+    e.preventDefault();
+  }
+  render(props) {
+    return (
+      <form className="login-form" onSubmit={this.handleSubmit}>
+        <div className="create-account-cta">
+          <div className="button pull-right" onClick={props.openModal.bind(this, 'signup')}>Create account</div>
+          <div>New here?</div>
+        </div>
+        <label htmlFor="email">Email address:</label>
+        <input type="email" autoFocus name="email" value={this.state.email}/>
+        <label htmlFor="password">Password:</label>
+        <input type="password" name="password" value={this.state.password}/>
+        <input type="submit" name="submit" value="Log in" />
+      </form>
+    );
+  }
+}
+
+export default LoginForm;
--- src/components/signup-form.js
+++ src/components/signup-form.js
@@ -1,13 +1,34 @@
 import React from 'react';
 
-export default () => (
-  <form className="login-form">
-    <label htmlFor="name">Display name:</label>
-    <input type="text" autoFocus name="name" />
-    <label htmlFor="email">Email address:</label>
-    <input type="email" name="email" />
-    <label htmlFor="password">Password:</label>
-    <input type="password" name="password" />
-    <input type="submit" name="submit" value="Create account" />
-  </form>
-);
+class SignupForm extends React.Component {
+  getInitialState() {
+    return {name: '', email: '', password: ''};
+  }
+  handleNameChange(e) {
+    this.setState({name: e.target.value});
+  }
+  handleEmailChange(e) {
+    this.setState({email: e.target.value});
+  }
+  handlePasswordChange(e) {
+    this.setState({password: e.target.value});
+  }
+  handleSubmit(e) {
+    e.preventDefault();
+  }
+  render() {
+    return (
+      <form className="login-form" onSubmit={this.handleSubmit}>
+        <label htmlFor="name">Display name:</label>
+        <input type="text" autoFocus name="name" value={this.state.name} onChange={this.handleNameChange} />
+        <label htmlFor="email">Email address:</label>
+        <input type="email" name="email" value={this.state.email} onChange={this.handleEmailChange} />
+        <label htmlFor="password">Password:</label>
+        <input type="password" name="password" value={this.state.password} onChange={this.handlePasswordChange} />
+        <input type="submit" name="submit" value="Create account" />
+      </form>
+    );
+  }
+}
+
+export default SignupForm;
