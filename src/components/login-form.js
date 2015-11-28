d25f5c958bbb6199f6871acbb7f298b4f7e804b6
Marcus Armstrong <marcusdarmstrong@gmail.com>
2015-11-28 12:51:15 -0500

    Create account crosssell

--- src/components/login-form.js
+++ src/components/login-form.js
@@ -2,6 +2,10 @@ import React from 'react';
 
 export default () => (
   <form className="login-form">
+    <div className="create-account-cta">
+      <div class="button pull-right">Create Account</div>
+      <div>New to cloth.io?</div>
+    </div>
     <label htmlFor="email">Email address:</label>
     <input type="email" autoFocus name="email" />
     <label htmlFor="password">Password:</label>
