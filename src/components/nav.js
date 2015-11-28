7e02da8e305a057322b317574b026cce6e90bce4
Marcus Armstrong <marcusdarmstrong@gmail.com>
2015-11-28 12:15:44 -0500

    Variable names are hard

--- src/components/nav.js
+++ src/components/nav.js
@@ -8,7 +8,7 @@ class Nav extends React.Component {
       modal = React.createElement(
         Modal,
         Object.assign({title: 'Log in'}, this.props),
-        React.createElement(modal)
+        React.createElement(this.props.modal)
       );
     }
 
