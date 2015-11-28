9a30246ec5c35267c4ca4e941bdee0c7cc5fac31
Marcus Armstrong <marcusdarmstrong@gmail.com>
2015-11-28 00:25:29 -0500

    Transfer properties

--- src/components/post.js
+++ src/components/post.js
@@ -2,16 +2,16 @@ import React from 'react';
 import Comments from './comments';
 import Nav from './nav';
 
-export default ({post, comments}) => (
+export default (props) => (
   <div>
-    <Nav />
+    <Nav {...props} />
     <div className="spacer">
       <div className="container">
         <article className="article">
-          <h1>{post.title}</h1>
-          <div dangerouslySetInnerHTML={{__html: post.body}}></div>
+          <h1>{props.post.title}</h1>
+          <div dangerouslySetInnerHTML={{__html: props.post.body}}></div>
         </article>
-        <Comments comments={comments} />
+        <Comments {...props} />
       </div>
     </div>
   </div>
