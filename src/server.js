d5547f86ebbb5443138b34d39c7eb67d2e00d0ae
Marcus Armstrong <marcusdarmstrong@gmail.com>
2015-11-28 18:50:53 -0500

    Added a bunch of auth stuff

--- src/server.js
+++ src/server.js
@@ -8,14 +8,17 @@ import { createStore } from 'redux';
 import { List as list, Map as map } from 'immutable';
 import { Provider } from 'react-redux';
 import compression from 'compression';
+import bodyParser from 'body-parser';
+import scrypt from 'scrypt';
+import cookieParser from 'cookie-parser';
 
 import layout from './layout';
 import sql from './sql';
 import commentOrdering from './comment-ordering';
 import App from './components/app';
 import reducer from './reducer';
-import { validate, NAME_REX, EMAIL_REX } from './validator';
-import { decodeAuthToken } from './auth-token';
+import { validate, NAME_REX, EMAIL_REX, PASSWORD_REX } from './validator';
+import { createAuthToken, decodeAuthToken } from './auth-token';
 
 const app = express();
 app.set('port', (process.env.PORT || 5000));
@@ -23,6 +26,9 @@ const server = app.listen(app.get('port'));
 SocketIO.listen(server);
 
 app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use('/public', express.static(path.join(__dirname, '..', 'public')));
 
 app.get('/', (req, res) => {
@@ -71,6 +77,52 @@ app.get('/api/isEmailTaken', (req, res) => {
   } else {
     res.send(JSON.stringify({success: false}));
   }
+});
+
+app.post('/api/createAccount', (req, res) => {
+  let name = req.body.name || '';
+  name = name.trim();
+  let email = req.body.email || '';
+  email = email.trim();
+  const password = req.body.password || '';
+
+  if (!name || !validate(NAME_REX, name)) {
+    return res.send(JSON.stringify({success: false, nameError: 'Invalid name'}));
+  }
+  if (!email || !validate(EMAIL_REX, email)) {
+    return res.send(JSON.stringify({success: false, emailError: 'Invalid email'}));
+  }
+  if (!password || !validate(PASSWORD_REX, password)) {
+    return res.send(JSON.stringify({success: false, passwordError: 'Invalid password'}));
+  }
+
+  const salt = new Buffer(name);
+  const passHash = scrypt.hashSync(password, {N: 16384, r: 8, p: 1}, 64, salt).toString('hex');
+  const color = passHash.substr(0, 6);
+
+  pg.connect(process.env.DATABASE_URL, (pgErr, client, done) => {
+    if (pgErr) {
+      return res.status(500).send('Internal Server Error');
+    }
+    client.query(sql`select id from t_user where email=${email}`, (emailErr, emailResult) => {
+      if (emailResult && emailResult.rows && emailResult.rows.length === 0) {
+        client.query(sql`select id from t_user where name=${name}`, (userErr, userResult) => {
+          if (userResult && userResult.rows && userResult.rows.length === 0) {
+            client.query(sql`insert into t_user (name, email, passhash, color) values (${name}, ${email}, ${passHash}, ${color}) returning id`, (err, result) => {
+              done();
+              const authToken = createAuthToken(result.rows[0].id);
+              res.cookie('auth', authToken, { maxAge: 60 * 60 * 24 * 365 * 2, httpOnly: true });
+              res.send(JSON.stringify({success: true}));
+            });
+          } else {
+            res.send(JSON.stringify({success: false, nameError: 'Name is already taken.'}));
+          }
+        });
+      } else {
+        res.send(JSON.stringify({success: false, emailError: 'Email is already taken.'}));
+      }
+    });
+  });
 });
 
 const articleMatcher = /\/p\/(.+)/;
