268b177183f0fd5d6189fa7af0af6a9e0e792ff4
Marcus Armstrong <marcusdarmstrong@gmail.com>
2015-11-27 19:27:49 -0500

    Adding an alert

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,8 +1,18 @@
 import React from 'react';
 
-export default () => (
-  <nav>
-    <div className="button pull-right" onClick="alert('hi');">Log in</div>
-    <a href="/"><img className="logo" src="/public/images/logo.png" /></a>
-  </nav>
-);
+class Nav extends React.Component {
+  openLogin() {
+    alert('hi');
+  }
+
+  render() {
+    return (
+      <nav>
+        <div className="button pull-right" onClick={this.openLogin}>Log in</div>
+        <a href="/"><img className="logo" src="/public/images/logo.png" /></a>
+      </nav>
+    );
+  }
+}
+
+export default Nav;
