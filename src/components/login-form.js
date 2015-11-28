94379a06aed4b8d2ce697e130250ab60559b4689
Marcus Armstrong <marcusdarmstrong@gmail.com>
2015-11-28 12:13:04 -0500

    Autofocus

--- src/components/login-form.js
+++ src/components/login-form.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default () => (
   <form className="login-form">
     <label htmlFor="email">Email address:</label>
-    <input type="email" name="email" />
+    <input type="email" autoFocus name="email" />
     <label htmlFor="password">Password:</label>
     <input type="password" name="password" />
     <input type="submit" name="submit" value="Log in" />
