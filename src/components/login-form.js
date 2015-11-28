254011f6d39b2cbf8859a70eb1c019f0cb867a40
Marcus Armstrong <marcusdarmstrong@gmail.com>
2015-11-28 13:12:01 -0500

    No this in the form

--- src/components/login-form.js
+++ src/components/login-form.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-export default () => (
+export default (props) => (
   <form className="login-form">
     <div className="create-account-cta">
-      <div className="button pull-right" onClick={this.props.openModal.bind(this, 'signup')}>Create account</div>
+      <div className="button pull-right" onClick={props.openModal.bind(this, 'signup')}>Create account</div>
       <div>New here?</div>
     </div>
     <label htmlFor="email">Email address:</label>
