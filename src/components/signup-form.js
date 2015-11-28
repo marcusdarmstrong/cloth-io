c897e9b022764ea04490a73c9b53b99ed75b5539
Marcus Armstrong <marcusdarmstrong@gmail.com>
2015-11-28 13:09:42 -0500

    Silly git

--- src/components/signup-form.js
+++ src/components/signup-form.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
 export default () => (
-  <form className="signup-form">
+  <form className="login-form">
     <label htmlFor="name">Display name:</label>
-    <input type="text" name="name" />
+    <input type="text" autoFocus name="name" />
     <label htmlFor="email">Email address:</label>
     <input type="email" name="email" />
     <label htmlFor="password">Password:</label>
     <input type="password" name="password" />
-    <input type="submit" name="submit" value="Sign up" />
+    <input type="submit" name="submit" value="Create account" />
   </form>
 );
