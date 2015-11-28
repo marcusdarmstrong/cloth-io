13c8bd3df17e263b8a179d0fffe70bc2366a4a30
Marcus Armstrong <marcusdarmstrong@gmail.com>
2015-11-28 01:02:22 -0500

    Modal

---
+++ src/components/modal.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default () => (
+  <div className="modal-screen">
+    <div className="modal-container">
+      {this.props.children}
+    </div>
+  </div>
+);
--- src/components/nav.js
+++ src/components/nav.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import Modal from './modal';
 
 class Nav extends React.Component {
   render() {
     const {modal, openModal} = this.props;
     return (
-      <nav>
-        <div className="button pull-right" onClick={openModal.bind(this, 'login')}>Log in</div>
-        <a href="/"><img className="logo" src="/public/images/logo.png" /></a>
-        {(modal) ? React.createElement(modal) : null}
-      </nav>
+      <div className="nav-container">
+        <nav>
+          <div className="button pull-right" onClick={openModal.bind(this, 'login')}>Log in</div>
+          <a href="/"><img className="logo" src="/public/images/logo.png" /></a>
+        </nav>
+        {(modal) ? React.createElement(Modal, this.props, React.createElement(modal)) : null}
+      </div>
     );
   }
 }
