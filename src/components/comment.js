1c33c53e01820c786f452252cc7ac776fae7ffa2
Marcus Armstrong <marcusdarmstrong@gmail.com>
2015-11-28 21:03:38 -0500

    Adding comment boxes on replies

--- src/components/comment.js
+++ src/components/comment.js
@@ -1,40 +1,70 @@
 import React from 'react';
 import TimeAgo from './time-ago';
+import AddCommentBox from './add-comment-box';
 
-export default ({comment}) => {
-  let classString = 'comment';
-  if (comment.hasReplies) {
-    classString += ' has-replies';
+class Comment extends React.Component {
+  constructor() {
+    super();
+    this.state = {commentBoxExpanded: false};
   }
-  if (comment.child) {
-    classString += ' child';
-  } else if (comment.fork) {
-    classString += ' fork';
+  toggleReplyBox() {
+    this.setState({commentBoxExpanded: true});
   }
-  const avatar = comment.name.charAt(0).toUpperCase();
-  const avatarColor = '#' + comment.color;
-  let markup = (<div className={classString}>
-    <div className="author"><div className="avatar" style={{backgroundColor: avatarColor}}>{avatar}</div></div>
-    <div className="comment-container">
-      <div className="comment-header">
-        <TimeAgo timestamp={comment.created} />
-        <div className="author-name">
-          {comment.name}
+  render() {
+    let classString = 'comment';
+    if (this.props.comment.hasReplies) {
+      classString += ' has-replies';
+    }
+    if (this.props.comment.child) {
+      classString += ' child';
+    } else if (this.props.comment.fork) {
+      classString += ' fork';
+    }
+    const avatar = this.props.comment.name.charAt(0).toUpperCase();
+    const avatarColor = '#' + this.props.comment.color;
+    let markup = (<div className={classString}>
+      <div className="author"><div className="avatar" style={{backgroundColor: avatarColor}}>{avatar}</div></div>
+      <div className="comment-container">
+        <div className="comment-header">
+          <TimeAgo timestamp={this.props.comment.created} />
+          <div className="author-name">
+            {this.props.comment.name}
+          </div>
         </div>
+        <div className="comment-text" dangerouslySetInnerHTML={{__html: this.props.comment.body}}></div>
+        <div className="comment-options">
+          <div className="button pull-right" onClick={this.toggleReplyBox.bind(this)}>Reply</div>
+        </div>
+        {(this.state.commentBoxExpanded) ? <AddCommentBox user={this.props.user} /> : null}
       </div>
-      <div className="comment-text" dangerouslySetInnerHTML={{__html: comment.body}}></div>
-      <div className="comment-options">
-        <div className="button pull-right">Reply</div>
-      </div>
-    </div>
-  </div>);
+    </div>);
 
-  let nestLevel = comment.nestLevel;
-  if (comment.child) {
-    --nestLevel;
+    let nestLevel = this.props.comment.nestLevel;
+    if (this.props.comment.child) {
+      --nestLevel;
+    }
+    for (let i = nestLevel; i > 0; i--) {
+      markup = (<div className="reply">{markup}</div>);
+    }
+    return markup;
   }
-  for (let i = nestLevel; i > 0; i--) {
-    markup = (<div className="reply">{markup}</div>);
-  }
-  return markup;
+}
+
+Comment.propTypes = {
+  comment: React.PropTypes.shape({
+    name: React.PropTypes.string,
+    color: React.PropTypes.string,
+    created: React.PropTypes.number,
+    body: React.PropTypes.string,
+    nestLevel: React.PropTypes.number,
+    hasReplies: React.PropTypes.bool,
+    fork: React.PropTypes.bool,
+    child: React.PropTypes.bool,
+  }).isRequired,
+  user: React.PropTypes.shape({
+    name: React.PropTypes.string,
+    color: React.PropTypes.string,
+  }),
 };
+
+export default Comment;
