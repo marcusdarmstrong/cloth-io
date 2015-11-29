3aafa4b4980e871599fead138510e508f61f3d51
Marcus Armstrong <marcusdarmstrong@gmail.com>
2015-11-28 23:53:18 -0500

    Passing openModal around

--- src/components/comment.js
+++ src/components/comment.js
@@ -41,9 +41,9 @@ class Comment extends React.Component {
         <div className="comment-options">
           <div className={replyState} onClick={this.toggleReplyBox.bind(this)}>Reply</div>
         </div>
-        {(this.state.commentBoxExpanded && this.props.comment.hasReplies) ? <AddCommentBox user={this.props.user} parentComment={this.props.comment} fork openModal={openModal} /> : null}
+        {(this.state.commentBoxExpanded && this.props.comment.hasReplies) ? <AddCommentBox user={this.props.user} parentComment={this.props.comment} fork openModal={this.props.openModal} /> : null}
       </div>
-      {(this.state.commentBoxExpanded && !this.props.comment.hasReplies) ? <AddCommentBox user={this.props.user} parentComment={this.props.comment} openModal={openModal} /> : null}
+      {(this.state.commentBoxExpanded && !this.props.comment.hasReplies) ? <AddCommentBox user={this.props.user} parentComment={this.props.comment} openModal={this.props.openModal} /> : null}
     </div>);
 
     let nestLevel = this.props.comment.nestLevel;
