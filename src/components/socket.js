e98bfc37472717e9e8e36f04a67b6a99c4f36e04
Marcus Armstrong <marcusdarmstrong@gmail.com>
2015-11-29 01:12:08 -0500

    Map doesn't work

--- src/components/socket.js
+++ src/components/socket.js
@@ -4,7 +4,11 @@ import React from 'react';
 // The only purpose of this component is to map socket.io events to redux actions.
 class Socket extends React.Component {
   componentDidMount() {
-    actions.map(action => this.props.socket.on(action, (data) => this.props[action](data)));
+    for (const action in actions) {
+      if (actions.hasOwnProperty(action)) {
+        this.props.socket.on(action, (data) => this.props[action](data));
+      }
+    }
   }
   render() {
     return null;
