6448f143dc06d55c74396ae4c896a269cefecb68
Marcus Armstrong <marcusdarmstrong@gmail.com>
2015-11-28 20:45:31 -0500

    User-based add comment

--- src/components/add-comment-box.js
+++ src/components/add-comment-box.js
@@ -1,20 +1,25 @@
 import React from 'react';
 
-export default () => (
-  <div className="add-comment-box">
-    <div className="author"><div className="avatar avatar-4">M</div></div>
-    <div className="add-comment-container">
-      <div className="comment-header">
-        <div className="author-name">
-          Marcus
+export default ({user}) => {
+  const color = '#' + user.color;
+  const letter = user.name.substr(0, 1).toUpperCase();
+  const avatar = (<div className="avatar" style={{backgroundColor: color}}>{letter}</div>);
+  return (
+    <div className="add-comment-box">
+      <div className="author">{avatar}</div>
+      <div className="add-comment-container">
+        <div className="comment-header">
+          <div className="author-name">
+            {user.name}
+          </div>
+        </div>
+        <div className="textarea-container">
+          <div className="textarea" contentEditable></div>
+        </div>
+        <div className="comment-options">
+          <div className="button pull-right">Post Comment</div>
         </div>
       </div>
-      <div className="textarea-container">
-        <div className="textarea" contentEditable></div>
-      </div>
-      <div className="comment-options">
-        <div className="button pull-right">Post Comment</div>
-      </div>
     </div>
-  </div>
-);
+  );
+};
