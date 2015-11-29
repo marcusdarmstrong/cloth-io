a81a88dc26379bd73a34e12b66efcc748ea24e14
Marcus Armstrong <marcusdarmstrong@gmail.com>
2015-11-28 20:47:49 -0500

    Transfering props

--- src/components/comments.js
+++ src/components/comments.js
@@ -2,11 +2,11 @@ import React from 'react';
 import AddCommentBox from './add-comment-box';
 import Comment from './comment';
 
-export default ({comments}) => (
+export default ({user, comments}) => (
   <section className="comments">
     <div className="comment-summary">
       <h2>{comments.length} {(comments.length === 1) ? 'Comment' : 'Comments'}</h2>
-      <AddCommentBox />
+      <AddCommentBox user={user} />
     </div>
     <div className="separator">&middot;&nbsp;&middot;&nbsp;&middot;</div>
     {comments.map((comment) =>
