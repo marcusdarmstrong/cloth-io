53ebe8f83b54b553539fb3c86fcb498a39a20533
Marcus Armstrong <marcusdarmstrong@gmail.com>
2015-12-01 15:42:56 -0500

    Type check

--- src/components/comments.js
+++ src/components/comments.js
@@ -6,7 +6,7 @@ export default ({post, user, comments, openModal}) => (
   <section className="comments">
     <div className="comment-summary">
       <h2>{comments.length} {(comments.length === 1) ? 'Comment' : 'Comments'}</h2>
-      <AddCommentBox user={user} openModal={openModal} postId={post.id} />
+      <AddCommentBox user={user} openModal={openModal} postId={Number(post.id)} />
     </div>
     <div className="separator">&middot;&nbsp;&middot;&nbsp;&middot;</div>
     {comments.map((comment) =>
