6b03945d6092935fd21ef7f9d4416b3ff9d86758
Marcus Armstrong <marcusdarmstrong@gmail.com>
2015-12-02 23:33:05 -0500

    Immutable

--- src/loaders/home.js
+++ src/loaders/home.js
@@ -1,14 +1,15 @@
 import { native as pg } from 'pg';
 import sql from '../sql';
+import { Map as map } from 'immutable';
 
 export default (cb) => {
   pg.connect(process.env.DATABASE_URL, (pgErr, client, done) => {
     client.query(sql`select * from t_post order by created desc limit 10`, (err, result) => {
       done();
       if (!err && result && result.rows) {
-        cb({ title: 'New York Jets / cloth.io', posts: result.rows });
+        cb(map({ title: 'New York Jets / cloth.io', posts: result.rows }));
       } else {
-        cb({ title: 'New York Jets / cloth.io - error' });
+        cb(map({ title: 'New York Jets / cloth.io - error' }));
       }
     });
   });
