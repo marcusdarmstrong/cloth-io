02195ac5d2cd39b36c755117bee97f3782efd902
Marcus Armstrong <marcusdarmstrong@gmail.com>
2015-12-02 21:25:40 -0500

    I'm stupid

--- src/router.js
+++ src/router.js
@@ -1,12 +1,12 @@
 const createHandler = (route, cb, stateMiddleware) => {
   return (req, res) => {
     if (route.pattern) {
-      const matches = route.pattern.exec(res.url);
+      const matches = route.pattern.exec(req.url);
       if (matches) {
         matches.shift();
         route.loader(state => stateMiddleware(req, state, cb.bind(null, res, route.component)), ...matches);
       } else {
-        res.status(500).send('Internal error: ' + res.url);
+        res.status(500).send('Internal error: ' + req.url);
       }
     } else {
       route.loader(state => stateMiddleware(req, state, cb.bind(null, res, route.component)));
