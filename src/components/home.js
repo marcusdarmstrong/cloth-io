9070d37d272e6ad15279f030dd8dc25d46170a36
Marcus Armstrong <marcusdarmstrong@gmail.com>
2015-12-02 23:35:37 -0500

    Corrected the link

--- src/components/home.js
+++ src/components/home.js
@@ -4,7 +4,7 @@ export default ({posts}) => {
   return (
     <div>
       <p>Hello World! This is the homepage.</p>
-      {posts.map(post => (<a href={post.urlstring}>{post.title}</a>))}
+      {posts.map(post => (<a href={'/p/' + post.urlstring}>{post.title}</a>))}
     </div>
   );
 };
