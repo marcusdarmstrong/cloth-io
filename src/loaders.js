2b9c127ac2a5f8542db18c2d492573ff77f7695b
Marcus Armstrong <marcusdarmstrong@gmail.com>
2015-12-11 14:15:24 -0500

    A real loader

--- src/loaders.js
+++ src/loaders.js
@@ -1,5 +1,7 @@
 import homeLoader from './loaders/home';
 import postLoader from './loaders/post';
+import shareLoader from './loaders/share';
+import { Map as map } from 'immutable';
 
 export default (routes) => {
   return routes.map(route => {
@@ -10,8 +12,11 @@ export default (routes) => {
     case 'Home':
       route.loader = homeLoader;
       break;
+    case 'Share':
+      route.loader = shareLoader;
+      break;
     default:
-      route.loader = () => ({'title': 'Share a Story'});
+      route.loader = (cb) => cb(map({title: 'cloth.io'}));
     }
     return route;
   });
---
+++ src/loaders/share.js
@@ -0,0 +1,5 @@
+import { Map as map } from 'immutable';
+
+export default (cb) => {
+  cb(map({ title: 'Share a Story / New York Jets / cloth.io' }));
+};
