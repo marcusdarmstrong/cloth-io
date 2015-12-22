6c240f1600fa0d95a0d076133827542cf93f44b1
Marcus Armstrong <marcusdarmstrong@gmail.com>
2015-12-22 18:59:46 -0500

    Minification + Smaller image

--- gulpfile.js
+++ gulpfile.js
@@ -38,9 +38,9 @@ gulp.task('client', ['clean-client'], function() {
     })
     .pipe(source('app.js'))
     .pipe(buffer())
-    //.pipe(sourcemaps.init({loadMaps: true}))
-    //  .pipe(uglify())
-    //.pipe(sourcemaps.write())
+    .pipe(sourcemaps.init({loadMaps: true}))
+      .pipe(uglify())
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest('public'));
 });
 
