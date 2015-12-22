0dfd344b721d921e0f35bbd92be6e087a5b659aa
Marcus Armstrong <marcusdarmstrong@gmail.com>
2015-12-22 14:32:28 -0500

    Adding a link

--- src/components/post.js
+++ src/components/post.js
@@ -1,26 +1,50 @@
 import React from 'react';
 import Comments from './comments';
 import Nav from './nav';
+import uriParser from '../uri-parser';
 
-const Post = (props) => (
-  <div>
-    <Nav {...props} />
-    <div className="spacer">
-      <div className="container">
-        <article className="article">
-          <h1>{props.post.title}</h1>
-          <div dangerouslySetInnerHTML={{__html: props.post.body}}></div>
-        </article>
-        <Comments {...props}/>
+const Post = (props) => {
+  const link = (props.post.url) ? uriParser(props.post.url).host : null;
+  if (link) {
+    return (
+      <div>
+        <Nav {...props} />
+        <div className="spacer">
+          <div className="container">
+            <article className="article">
+              <a href={props.post.url} target="_new" className="headline-link">
+                <h1>{props.post.title}</h1>
+                <span className="link-desc">{link} &crarr;</span>
+              </a>
+              <div dangerouslySetInnerHTML={{__html: props.post.body}}></div>
+            </article>
+            <Comments {...props}/>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  return (
+    <div>
+      <Nav {...props} />
+      <div className="spacer">
+        <div className="container">
+          <article className="article">
+            <h1>{props.post.title}</h1>
+            <div dangerouslySetInnerHTML={{__html: props.post.body}}></div>
+          </article>
+          <Comments {...props}/>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 Post.propTypes = {
   post: React.PropTypes.shape({
     title: React.PropTypes.string,
     body: React.PropTypes.string,
+    url: React.PropTypes.string,
   }),
 };
 
