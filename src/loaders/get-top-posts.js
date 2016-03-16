29071cc6872ea14aba85685b0d4a900ca583cc83
Marcus Armstrong <marcusdarmstrong@gmail.com>
2016-03-16 18:47:09 +0100

    more

--- src/loaders/get-top-posts.js
+++ src/loaders/get-top-posts.js
@@ -3,8 +3,9 @@ import sql from '../util/sql';
 import onError from '../util/on-error';
 
 export default onError(async function getTopPosts(db = connect()) {
-  console.log('db: ' + Object.keys(db) + ' json ' + JSON.stringify(db));
-  console.log('conn: ' + Object.keys(connect) + ' json ' + connect.toString());
+  console.log('args: ' + JSON.stringify(arguments, (k, v) => (v === undefined) ? 'undefined' : v));
+  console.log('db: ' + JSON.stringify(db, (k, v) => (v === undefined) ? 'undefined' : v));
+  console.log('conn: ' + connect.toString());
   return await db.query(
     sql`select p.*, u.name, u.color, cc.commentCount from t_post p join t_user u on u.id = p.user_id left join (select c.post_id, count(*) as commentCount from t_comment c group by post_id) as cc on cc.post_id = p.id order by created desc limit 10`
   );
