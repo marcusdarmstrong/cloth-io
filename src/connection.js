d0b561a496ab4bec125a0a517ea317ead977555d
Marcus Armstrong <marcusdarmstrong@gmail.com>
2016-03-16 18:37:38 +0100

    and again

--- src/connection.js
+++ src/connection.js
@@ -1,7 +1,7 @@
 import configurable from 'pg-promise';
 
 const pgp = configurable({});
-
+console.log('wtf');
 export default () => {
   const db = pgp(process.env.DATABASE_URL);
   console.log('DB created: ' + JSON.stringify(Object.keys(db)));
--- src/loaders/get-top-posts.js
+++ src/loaders/get-top-posts.js
@@ -3,6 +3,8 @@ import sql from '../util/sql';
 import onError from '../util/on-error';
 
 export default onError(async function getTopPosts(db = connect()) {
+  console.log('db: ' + Object.keys(db) + ' json ' + JSON.stringify(db));
+  console.log('conn: ' + Object.keys(connect) + ' json ' + connect.toString());
   return await db.query(
     sql`select p.*, u.name, u.color, cc.commentCount from t_post p join t_user u on u.id = p.user_id left join (select c.post_id, count(*) as commentCount from t_comment c group by post_id) as cc on cc.post_id = p.id order by created desc limit 10`
   );
