b7af9f00e5b07e4fb8146f8558db30e39d01fd17
Marcus Armstrong <marcusdarmstrong@gmail.com>
2016-03-16 13:33:55 +0100

    Idk

--- src/handlers/home.js
+++ src/handlers/home.js
@@ -1,11 +1,12 @@
 import { Map as map } from 'immutable';
 import getTopPosts from '../loaders/get-top-posts';
 import Home from '../components/home';
+import connect from '../connection';
 
 export default async () => ({
   state: map({
     title: 'New York Jets / cloth.io',
-    posts: await getTopPosts(),
+    posts: await getTopPosts(connect()),
   }),
   component: Home,
 });
