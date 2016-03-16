8129d0034bbdc7868d7d06be6d1995d56bd88a64
Marcus Armstrong <marcusdarmstrong@gmail.com>
2016-03-16 22:32:56 +0100

    BLah

--- src/util/on-error.js
+++ src/util/on-error.js
@@ -1,7 +1,7 @@
 export default function onError(func, value = null) {
   return async (...args) => {
     try {
-      return await func(...args);
+      return await func.apply(args);
     } catch (e) {
       console.error(e.stack);
     }
