5eadcf850898adee0d99e6a19814a93cdd70eb7b
Marcus Armstrong <marcusdarmstrong@gmail.com>
2016-03-16 13:02:18 +0100

    Trying an alias

--- src/loaders/get-top-posts.js
+++ src/loaders/get-top-posts.js
@@ -3,7 +3,7 @@ import sql from '../util/sql';
 import onError from '../util/on-error';
 
 export default onError(async function getTopPosts(db = connect()) {
-  return await db.any(
+  return await db.query(
     sql`select p.*, u.name, u.color, cc.commentCount from t_post p join t_user u on u.id = p.user_id left join (select c.post_id, count(*) as commentCount from t_comment c group by post_id) as cc on cc.post_id = p.id order by created desc limit 10`
   );
 }, []);
