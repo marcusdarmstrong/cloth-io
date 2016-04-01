5dc623307e21726942d9c62e2b9d488273ac8b5e
Marcus Armstrong <marcusdarmstrong@gmail.com>
2016-04-01 19:49:58 -0400

    More logs

--- src/util/on-error.js
+++ src/util/on-error.js
@@ -1,6 +1,10 @@
 export default function onError(func, value = null) {
   return async (...args) => {
     try {
+      console.error(`In the error a call: ${JSON.stringify(args)}`);
+      // eslint-disable-next-line
+      console.error(`In the error b call: ${JSON.stringify(arguments)}`);
+
       return await func.apply(args);
     } catch (e) {
       console.error(e.toString());
