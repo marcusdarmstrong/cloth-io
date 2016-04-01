c667e28c67d22e91424ef19defc96d5e132fe88e
Marcus Armstrong <marcusdarmstrong@gmail.com>
2016-04-01 19:47:32 -0400

    More logs

--- src/loaders/get-post-by-urlstring.js
+++ src/loaders/get-post-by-urlstring.js
@@ -3,7 +3,7 @@ import onError from '../util/on-error';
 
 export default onError(async function getPostByUrlString(urlString, db = connect()) {
   // eslint-disable-next-line
-  console.error(`In the cb call: ${arguments}`);
+  console.error(`In the cb call: ${JSON.stringify(arguments)}`);
   return await db.one(
     'select * from t_post where urlstring=$1',
     urlString
