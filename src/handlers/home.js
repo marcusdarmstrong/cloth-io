42517dcd9ffab120647b3cb2622d27f8731ece11
Marcus Armstrong <marcusdarmstrong@gmail.com>
2016-04-01 20:10:47 -0400

    Logging you in on the homepage

--- src/handlers/home.js
+++ src/handlers/home.js
@@ -1,11 +1,19 @@
 import { Map as map } from 'immutable';
 import getTopPosts from '../loaders/get-top-posts';
+import getUserById from '../loaders/get-user-by-id';
+import { readAuthTokenFromCookies } from '../auth-token';
 import Home from '../components/home';
+import connect from '../connect';
 
-export default async () => ({
-  state: map({
-    title: 'New York Jets / cloth.io',
-    posts: await getTopPosts(),
-  }),
-  component: Home,
-});
+export default async (req) => {
+  const db = connect();
+
+  return {
+    state: map({
+      title: 'New York Jets / cloth.io',
+      posts: await getTopPosts(db),
+      user: await getUserById(readAuthTokenFromCookies(req), db),
+    }),
+    component: Home,
+  };
+};
