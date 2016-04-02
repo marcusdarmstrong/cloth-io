816155ad11786b65cf60b4a8385e6804b15dea9c
Marcus Armstrong <marcusdarmstrong@gmail.com>
2016-04-02 11:56:42 -0400

    Whoops

--- src/loaders/get-post-by-urlstring.js
+++ src/loaders/get-post-by-urlstring.js
@@ -2,13 +2,11 @@ import connect from '../connection';
 import onError from '../util/on-error';
 
 export default onError(async function getPostByUrlString(urlString, db = connect()) {
-  return (await db.one(
+  const post = await db.one(
     'select * from t_post where urlstring=$1',
     urlString
-  )).map(post => {
-    const newPost = post;
-    newPost.id = Number(newPost.id);
-    newPost.created = Number(newPost.created);
-    return newPost;
-  });
+  );
+  post.id = Number(post.id);
+  post.created = Number(post.created);
+  return post;
 });
