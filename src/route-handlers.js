a3a91596b450430046647ebd6f10561ca9826853
Marcus Armstrong <marcusdarmstrong@gmail.com>
2016-04-03 14:26:31 -0400

    Removing this

--- src/route-handlers.js
+++
@@ -1,9 +0,0 @@
-import Home from './handlers/home';
-import Post from './handlers/post';
-import Share from './handlers/share';
-
-export default {
-  '/': Home,
-  '/p/:urlString': Post,
-  '/share': Share,
-};
