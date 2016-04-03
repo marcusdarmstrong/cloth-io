335d1fd239dc1665b8fa77d9c6932e6b1d76b5cf
Marcus Armstrong <marcusdarmstrong@gmail.com>
2016-04-03 08:14:45 -0400

    Fixing a race condition

--- src/components.js
+++
@@ -1,9 +0,0 @@
-import HomeComp from './components/home';
-import PostComp from './components/post';
-import ShareComp from './components/share';
-
-export default {
-  Share: ShareComp,
-  Post: PostComp,
-  Home: HomeComp,
-};
