568c7dd9f8b406dbbf810b315f2083525a8cb328
Marcus Armstrong <marcusdarmstrong@gmail.com>
2016-04-03 19:26:25 -0400

    Actually pull the data

--- src/loaders/get-post-by-urlstring.js
+++ src/loaders/get-post-by-urlstring.js
@@ -3,10 +3,18 @@ import onError from '../util/on-error';
 
 export default onError(async function getPostByUrlString(urlString, db = connect()) {
   const post = await db.one(
-    'select * from t_post where urlstring=$1',
+    `select p.*, u.name, u.color
+      from t_post p join t_user u on u.id = p.user_id
+      where urlstring=$1`,
     urlString
   );
   post.id = Number(post.id);
   post.created = Number(post.created);
+  post.user = {
+    name: post.name,
+    color: post.color,
+  };
+  delete post.name;
+  delete post.color;
   return post;
 });
