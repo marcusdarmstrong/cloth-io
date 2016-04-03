95c93fcc599048f60278a689aaa40dce76a94cc8
Marcus Armstrong <marcusdarmstrong@gmail.com>
2016-04-03 19:17:55 -0400

    Data for attribution

--- src/components/post.js
+++ src/components/post.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Comments from './comments';
 import Nav from './nav';
 import uriParser from '../util/uri-parser';
+import Avatar from './avatar';
+import TimeAgo from './time-ago';
 
 const Post = (props) => {
   const link = (props.post.url) ? uriParser(props.post.url).host : null;
@@ -21,6 +23,13 @@ const Post = (props) => {
         <div className="container">
           <article className="article">
             {title}
+            <div className="post-attribution">
+              <Avatar name={props.post.user.name} hex={props.post.user.color} />
+              {props.post.user.name}
+              <span className="post-time">
+                <TimeAgo timestamp={props.post.created} />
+              </span>
+            </div>
             <div dangerouslySetInnerHTML={{ __html: props.post.body }}></div>
           </article>
           <Comments {...props} />
@@ -32,9 +41,14 @@ const Post = (props) => {
 
 Post.propTypes = {
   post: React.PropTypes.shape({
-    title: React.PropTypes.string,
-    body: React.PropTypes.string,
+    title: React.PropTypes.string.isRequired,
+    body: React.PropTypes.string.isRequired,
     url: React.PropTypes.string,
+    created: React.PropTypes.number.isRequired,
+    user: React.PropTypes.shape({
+      name: React.PropTypes.string.isRequired,
+      color: React.PropTypes.string.isRequired,
+    }),
   }),
 };
 
