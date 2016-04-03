ee4c6ec45b57ccbd38bc0e56f800209e8c358d5a
Marcus Armstrong <marcusdarmstrong@gmail.com>
2016-04-03 13:27:55 -0400

    Minor fix

--- src/components/app.js
+++ src/components/app.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import Route from './route';
+import Router from './router';
 import Post from './post';
 import Home from './home';
 import Share from './share';
 
 const App = ({ store }) =>
   (<Provider store={store}>
-    <Route name="Post">
-      <Post />
-    </Route>
-    <Route name="Share">
-      <Share />
-    </Route>
-    <Route name="Home">
-      <Home />
-    </Route>
+    <Router>
+      <Route name="Post">
+        <Post />
+      </Route>
+      <Route name="Share">
+        <Share />
+      </Route>
+      <Route name="Home">
+        <Home />
+      </Route>
+    </Router>
   </Provider>);
 
 App.propTypes = {
