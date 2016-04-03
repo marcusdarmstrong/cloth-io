942db82e75b8c0c2918891a802c9c0531882da87
Marcus Armstrong <marcusdarmstrong@gmail.com>
2016-04-03 14:00:47 -0400

    Removing deprecated files

--- src/routes.js
+++
@@ -1,9 +0,0 @@
-import Home from './components/home';
-import Post from './components/post';
-import Share from './components/share';
-
-export default [
-  { name: 'Share', path: '/share', component: Share },
-  { name: 'Post', pattern: /\/p\/(.+)/, component: Post },
-  { name: 'Home', path: '/', component: Home },
-];
