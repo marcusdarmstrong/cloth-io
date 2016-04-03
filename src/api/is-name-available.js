e96efd038165949d1a46422a373ff7e6a4764912
Marcus Armstrong <marcusdarmstrong@gmail.com>
2016-04-03 11:02:07 -0400

    Bug with signup

--- src/api/is-name-available.js
+++ src/api/is-name-available.js
@@ -5,7 +5,7 @@ import { validate, NAME_REX } from '../validator';
 export default onError(async function isNameAvailable(req, res) {
   const name = req.query.name;
   if (name && validate(NAME_REX, name)) {
-    return res.json({ success: !!(await getUserByName(name.trim())) });
+    return res.json({ success: !(await getUserByName(name.trim())) });
   }
   return res.json({ success: false });
 }, (req, res) => res.status(500).json({ success: false }));
